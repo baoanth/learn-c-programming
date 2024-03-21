119dfc8435989b418b2620f768cde0a489ea1911
baoanth <baoanth@gmail.com>
2024-03-21 08:03:48 +0700

    GLOB only test cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,8 +2,7 @@
 
 // Include your test files here
 
-
-int main(int argc, char **argv) {
+/*int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
+}*/
